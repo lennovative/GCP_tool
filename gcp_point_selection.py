148d3f337343865b0830e6b7cd305be6d218c738
Lennart <lennart.petersen@online.de>
2022-03-04 07:31:41 +0100

    working

--- gcp_point_selection.py
+++ gcp_point_selection.py
@@ -12,7 +12,7 @@ def show_image(filename, window_name):
 def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN and not params[0]:
         params[0] = True
-        params[1] += f"\n{x} {y} "
+        params[1] += f"{x} {y} "
 
 def main():
     a = sys.argv
@@ -32,32 +32,40 @@ def main():
     window_name = "GCP" #f"GCP for image {image_name} ({index}/{n})"
     window_size=(700, 1700)
     waypoints = pd.read_csv("./waypoints.csv", index_col="label")
+    print(waypoints)
+    #exit()
     
     params = [False, "WGS84 UTM 32N"]
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, window_size[1], window_size[0])
     cv2.setMouseCallback(window_name, click_event, params)
 
+    new_entry_prepared = False
     index = 0
     while(index < n):
         filename = filenames[index]
-        waypoint = filename.split("\\")[-2]
+        if not new_entry_prepared:
+            wp = filename.split("\\")[-2]
+            data = dict(waypoints.loc[wp])
+            params[1] += "\n{} {} {} ".format(data["lat"], data["long"], data["ele_correc"])
+            print("({}/{}) Waypoint {}: {}".format(str(index+1), str(n), wp, data["comment"]))
+            new_entry_prepared = True
         show_image(filename, window_name)
         if params[0]:
             params[1] += filenames[index].split("\\")[-1]
-            print("(" + str(index+1) + "/" + str(n) + ") " + params[1].split("\n")[-1])
+            print(params[1].split("\n")[-1])
             params[0] = False
+            new_entry_prepared = False
             index += 1
         else:
             k = cv2.waitKey(10)
             if k == 27: #Esc
                 break
-            elif k == 8 and index > 0: #Backspace
+            elif k == 8 and index > 0 and not params[0]: #Backspace
                 print("delete point " + str(index))
                 index -= 1
                 s = params[1]
-                params[1] = s[:s.rfind('\n')]
-
+                new_entry_prepared = False
     cv2.destroyAllWindows()
     print(params[1])
     result_path = "./gcp_file.txt"
